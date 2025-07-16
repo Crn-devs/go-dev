efe4909a1afde37c624d9578b60fd0a980d6d208
connor <crn.develops@gmail.com>
2025-07-16 01:32:44 +0100

    variables in go: deep dive

--- variables/main.go
+++ variables/main.go
@@ -9,7 +9,15 @@ import "fmt"
 // string ""
 // pointers, slices, maps, functions are all assigned nil for zero value
 // arrays and structs get all fields or elements initalised with zero values
-//
+
+// ----scope----
+// in go scope goes as follows
+// variables declared outside main or any function are package level variables
+// if exported with a capital they are available throughout the package
+// globally and to any packages that import the package they are in
+
+// variables declared inside functions including main are only scoped to that function
+// "function-scoped" only accessable to that function and cannot be accessed from outside
 
 func main() {
 
@@ -55,4 +63,12 @@ func main() {
 	fmt.Println(p1)   // zero's to person{fname: "", lName: ""}
 	fmt.Println(pArr) // array of zero value persons
 
+	functionVariables() // variables such as person1 declared inside a function cannot be accessed by other functions unless passed
+
+}
+
+func functionVariables() {
+	person1 := struct{ name string }{name: "john"} // only accessable inside the function body
+
+	fmt.Println(person1)
 }
