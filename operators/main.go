fc237a66f3353bf1c0c2b17a2592bae002ac32b3
connor <crn.develops@gmail.com>
2025-07-27 01:32:53 +0100

    quick review of operators in go

---
+++ operators/main.go
@@ -0,0 +1,36 @@
+package main
+
+func main() {
+
+	/** Logical operators
+
+	(!) NOT
+	(&&) AND
+	(||) OR
+
+	**/
+
+	/** Bitwise operators
+
+	(&) bitwise AND
+	(|) bitwise OR
+	(^) bitwise XOR
+	(&^) bitwise, AND NOT
+	(<<) left shift
+	(>>) right shift
+
+	**/
+
+	/** Comparrison operators
+
+	(==) equal
+	(!=) not equal
+	(<) less than
+	(<=) less than or equal to
+	(>) greater than
+	(>=) greater than or equal to
+
+
+	**/
+
+}
